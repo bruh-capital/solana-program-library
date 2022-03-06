ac41ec84aba50a37bc9635d3d22bf177f8dac416
0xMillionz <madokakaname99@protonmail.com>
2022-03-06 17:02:28 -0500

    bruh?

--- token-swap/program/src/state.rs
+++ token-swap/program/src/state.rs
@@ -88,7 +88,7 @@ impl SwapVersion {
 
 /// Program states.
 #[repr(C)]
-#[derive(Debug, Default, PartialEq)]
+#[derive(Debug, Default, PartialEq, Clone)]
 pub struct SwapV1 {
     /// Initialized state.
     pub is_initialized: bool,
